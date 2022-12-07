ab8ce4b0fb5aaabeb4c32634ca95c2b440c0c2e2
gscott58 <31557074+gscott58@users.noreply.github.com>
2022-12-07 14:33:30 -0600

    Initial Load (Scott Family Christmas Card)

---
+++ js/main.js
@@ -0,0 +1,21 @@
+    var d = new Date();
+    var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    document.getElementById("copyright-1").innerHTML = "<strong>&copy; Copyright " + d.getFullYear() + "</strong>";
+
+    $(document).ready(function() {
+        $("#title").toggle(2000);
+        $(".adv-custom-caption").toggle(2000);
+        $("#contact-name").toggle(1500);
+        $("#contact-email").toggle(2000);
+        $("#copyright-1").toggle(2500);
+        $("#copyright-2").toggle(3000);
+                        
+        $("footer").hover(function() {         
+            $("#contact-name").effect("shake");
+            $("#contact-email").effect("shake");
+            }, function() {
+            $("#copyright-1").effect("shake");
+            $("#copyright-2").effect("shake");
+        });
+        
+    });
