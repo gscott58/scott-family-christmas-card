d915ba76c378d34c493485e2ff3d8fed5e080abf
gscott58 <31557074+gscott58@users.noreply.github.com>
2022-12-24 12:49:28 -0600

    Revised With Photo Elements 2023

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@
         $("#title").toggle(2000);
         $(".adv-custom-caption").toggle(2000);
         $("#contact-name").toggle(1500);
-        $("#contact-email").toggle(2000);
+      /*$("#contact-email").toggle(2000);*/
         $("#copyright-1").toggle(2500);
         $("#copyright-2").toggle(3000);
                         
